576b3aa729a7763a4b383fec9d38c4995ce9ed04
Logan Reynolds <logan.reynolds@remote-learner.net>
2017-12-18 22:38:44 -0700

    DATAHUB-1688: Updated to version 2017111300 release 3.4.0.0

--- plugins/etl/version.php
+++ plugins/etl/version.php
@@ -2,6 +2,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017051501;
-$plugin->release = '3.3.0.1';
+$plugin->version = 2017111300;
+$plugin->release = '3.4.0.0';
 $plugin->component = 'eliscore_etl';
--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017051500;
-$plugin->requires = 2017051500;
-$plugin->release = '3.3.0.1';
+$plugin->version = 2017111300;
+$plugin->requires = 2017111300;
+$plugin->release = '3.4.0.0';
 $plugin->component = 'local_eliscore';
 $plugin->maturity = MATURITY_STABLE;
 
